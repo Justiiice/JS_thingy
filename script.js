6700ae01948d51e055f14d34837320928ae1aeb2
Justiiice <32276752+Justiiice@users.noreply.github.com>
2022-01-09 20:37:43 +0100

    initial commit

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 var staticArray = ["Dogs", "Cats", "Birds", "Fish", "Dinosaurs", "Rabbits"];
 var array = ["Dogs", "Cats", "Birds", "Fish", "Dinosaurs", "Rabbits"];
-let list = document.getElementById("list");
+const list = document.getElementById("list");
 
 function printList(){
   var randomNum = Math.floor(Math.random() * array.length)
   if(!array.length) return
-    list.innerHTML += "<li>" + array[randomNum] + "</li>";
-    array.splice(randomNum, 1);
+  list.innerHTML += "<li>" + array[randomNum] + "</li>";
+  array.splice(randomNum, 1);
 }
 
 function reset(){
